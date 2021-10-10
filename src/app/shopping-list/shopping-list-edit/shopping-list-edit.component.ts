4973daa4f42c3cb70525ed6192e9a34dacbfb2a4
Momen Arabi <momenarabi25@gmail.com>
2021-10-11 00:01:35 +0200

    added & updated paths, added shoppinglist service, fixed functions

--- src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-import { Ingredient } from '../../shared/ingredient.model'
+// import { Ingredient } from '../../shared/ingredient.model'; (no need to import Ingredient model after using the ShoppingListService)
+import { ShoppingListService } from '../../shared/shopping-list.service';
 
 @Component({
   selector: 'app-shopping-list-edit',
@@ -11,17 +12,19 @@ export class ShoppingListEditComponent implements OnInit {
   ingName: string;
   ingAmount: number;
 
-  @Output() ingDetails= new EventEmitter<Ingredient>();
+  //@Output() ingDetails= new EventEmitter<Ingredient>(); (old event emmitter to send ingredient details to shopping list component)
 
-  onIngredientAdd(ing: Ingredient) {
-    ing.name = this.ingName;
-    console.log(ing.name);
-    ing.amount = this.ingAmount;
-    console.log(ing.amount);
-    this.ingDetails.emit(ing);
+  constructor(private shoppingListService: ShoppingListService) {}
+
+  onIngredientAdd() {
+    this.shoppingListService.addIngredient(this.ingName, this.ingAmount);
+    // old click event to send new added ingredient name and amount to shopping list component
+      /*ing.name = this.ingName;
+      console.log(ing.name);
+      ing.amount = this.ingAmount;
+      console.log(ing.amount);
+      this.ingDetails.emit(ing);*/
   }
-
-  constructor() { }
 
   ngOnInit(): void {
   }
