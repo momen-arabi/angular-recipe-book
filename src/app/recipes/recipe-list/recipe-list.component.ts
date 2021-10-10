73d4bfbf5bce5dd69f737ecc04dc537cc4b6255c
Momen Arabi <momenarabi25@gmail.com>
2021-10-10 23:54:04 +0200

    added & updated paths, added recipe service, fixed functions and class

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-import { Recipe } from '../recipes.model';
+import { Recipe } from '../../shared/recipes.model';
+import { RecipesService } from '../../shared/recipes.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,21 +9,26 @@ import { Recipe } from '../recipes.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeDetails = new EventEmitter<Recipe>();
+  // @Output() recipeDetails = new EventEmitter<Recipe>();
 
-  recipes: Recipe[] = [
+  recipes: Recipe[] = [];
+
+  // old recipe list array
+  /*recipes: Recipe[] = [
     new Recipe('Recipe 1', 'This is the recipe 1 description', 'https://static.onecms.io/wp-content/uploads/sites/44/2021/02/18/veggie-grilled-cheese-tomato-soup.jpg'),
     new Recipe('Recipe 2', 'This is the recipe 2 description', 'https://assets.bonappetit.com/photos/60a4022a248102a01bcfa0b6/1:1/w_2560%2Cc_limit/0621-Sheet-Pan-Gnocchi.jpg')
-  ];
+  ];*/
 
-  onSentToList(listDetails: Recipe) {
+  // old function for event binding recipe list component -> recipe component
+  /*onSentToList(listDetails: Recipe) {
     this.recipeDetails.emit(listDetails);
     console.log("Recipe Sent To List");
-  }
+  }*/
 
-  constructor() { }
+  constructor(private recipesService: RecipesService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.recipes = this.recipesService.recipes;
   }
 
 }
