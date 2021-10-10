019713a49d359f7b05e859a5d46d454b6e23d850
Momen Arabi <momenarabi25@gmail.com>
2021-10-10 23:52:23 +0200

    added & updated paths, removed event emitter, added recipe service

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
 
-import { Recipe } from '../../recipes.model';
+import { Recipe } from '../../../shared/recipes.model';
+import { RecipesService } from '../../../shared/recipes.service';
 
 @Component({
   selector: 'app-recipe-item',
@@ -10,13 +11,15 @@ import { Recipe } from '../../recipes.model';
 export class RecipeItemComponent implements OnInit {
   @Input() recipeItem: Recipe;
 
-  @Output() recipeToList = new EventEmitter<Recipe>();
+  //@Output() recipeToList = new EventEmitter<Recipe>(); (old event emmitter to send the recipe to the recipe list component)
+
+  constructor(private recipesService: RecipesService) {}
 
   onRecipeItemClick() {
-    this.recipeToList.emit(this.recipeItem);  
+    //this.recipeToList.emit(this.recipeItem); (emitting the recipe item to the recipe list component on recipe item click)
+    this.recipesService.recipeViewed.emit(this.recipeItem);
+    console.log("Recipe Item is:" + this.recipeItem.name);
   }
-
-  constructor() { }
 
   ngOnInit(): void {
   }
